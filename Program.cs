7dff17ffe86b3a96d83bf7a56be31f65b79ad124
RokyZevon <12629919+RokyZevon@users.noreply.github.com>
2024-02-16 00:34:02 +0800

    feat: Implemented basic conversion between MessagePack and JSON, with wildcard support

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using MessagePack;
+
+if (args.Length != 1)
+{
+    Console.WriteLine("Usage: MsgPacker <filepath>");
+    Environment.Exit(1);
+}
+
+var files = Directory.GetFiles(
+    Path.GetDirectoryName(args[0]) ?? Environment.CurrentDirectory,
+    Path.GetFileName(args[0]));
+
+if (files.Length == 0)
+{
+    Console.WriteLine("No files found");
+    Environment.Exit(1);
+}
+
+var outputDir = Path.Combine(Environment.CurrentDirectory, "output");
+if (!Directory.Exists(outputDir)) Directory.CreateDirectory(outputDir);
+
+foreach (var file in files)
+{
+    var (name, ext) = (Path.GetFileNameWithoutExtension(file), Path.GetExtension(file).ToLower());
+    string outputPath;
+
+    switch (ext)
+    {
+        case ".msg":
+            outputPath = Path.Combine(outputDir, $"{name}.json");
+            var bytes = await File.ReadAllBytesAsync(file);
+            await File.WriteAllTextAsync(outputPath, MessagePackSerializer.ConvertToJson(bytes));
+            Console.WriteLine($"Converted {name}.msg to {outputPath} ");
+            break;
+
+        case ".json":
+            outputPath = Path.Combine(outputDir, $"{name}.msg");
+            var text = await File.ReadAllTextAsync(file);
+            File.WriteAllBytes(outputPath, MessagePackSerializer.ConvertFromJson(text));
+            Console.WriteLine($"Converted {name}.json to {outputPath} ");
+            break;
+
+        default:
+            Console.WriteLine("Invalid file extension");
+            Environment.Exit(1);
+            break;
+    }
+}
+
+Console.WriteLine("All files have been converted successfully.");
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # MsgPacker
+
+A simple CLI tool that uses [MessagePack-CSharp](https://github.com/MessagePack-CSharp/MessagePack-CSharp) to convert between MessagePack and JSON, with wildcard support.
+
+## Usage
+
+```shell
+MsgPacker <filepath>
+```
+
+### Examples
+
+```shell
+MsgPacker ./text.msg
+```
+
+will convert `./text.msg` to `./output/text.json`.
+
+```shell
+MsgPacker ./*.msg
+```
+
+will convert all `.msg` files in the current directory to `.json` files in the `./output` directory.
